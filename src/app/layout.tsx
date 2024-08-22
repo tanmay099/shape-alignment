4a2cb2d023221baaf3649d0b8d922f3c27dd79bf
Tanmay Sharma <tanmay.sharma@optym.com>
2024-08-22 13:05:49 +0530

    added g tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MJY67WS9TL"
+        />
+
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MJY67WS9TL'});
+          `}
+        </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
